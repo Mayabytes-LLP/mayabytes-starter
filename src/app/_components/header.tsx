ff3af83814d10ece309dcf964f6fb14d17364688
Abdul Rafay Shaikh <abdul.rafay@mayabytes.com>
2024-04-18 22:46:59 +0500

    Update package name and add turbo flag to dev script in package.json

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,23 +4,14 @@ export default function Header({
   children,
 }: Readonly<{ children?: React.ReactNode }>) {
   return (
-    <header className="mx-auto flex w-full max-w-4xl items-center justify-between px-4 py-8">
-      <a href="/" className="text-xl font-bold">
-        Next.js
+    <header className="mx-auto flex w-full items-center justify-between px-4 py-8">
+      <a href="/" className="text-xl font-bold text-rose-400">
+        Mayabytes
       </a>
-      <nav>
-        <a href="/docs">Docs</a>
-        <a href="/examples">Examples</a>
-        <a href="/learn">Learn</a>
-        <a href="/showcase">Showcase</a>
-        <a href="/blog">Blog</a>
-        <a href="/community">Community</a>
-        <a href="/commerce">Commerce</a>
-      </nav>
+      <nav className="flex flex-wrap justify-center gap-2">{children}</nav>
       <div>
         <ModeToggle />
       </div>
-      {children}
     </header>
   )
 }
