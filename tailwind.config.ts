07175061a43ed631d1fd8d46dc30053d9f2f063a
Abdul Rafay Shaikh <abdul.rafay@mayabytes.com>
2024-07-08 23:17:34 +0500

    fix tailwindcss breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,17 +7,21 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
     },
     extend: {
+      theme: {
+        extend: {
+          screens: {
+            "3xl": "1920px",
+          },
+        }
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
